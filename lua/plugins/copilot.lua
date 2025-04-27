b3a60836f98186f15532da57c4e87d3dbd778bb7
Colin Graydon <colingraydon@gmail.com>
2025-04-27 12:05:58 -0400

    feat: enable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,17 +1,26 @@
-opts = {
-  suggestion = {
-    enabled = not vim.g.ai_cmp,
-    auto_trigger = true,
-    hide_during_completion = vim.g.ai_cmp,
-    keymap = {
-      accept = false, -- handled by nvim-cmp / blink.cmp
-      next = "<M-]>",
-      prev = "<M-[>",
-    },
-  },
-  panel = { enabled = false },
-  filetypes = {
-    markdown = true,
-    help = true,
+-- In your LazyVim plugins file (like lua/plugins/copilot.lua)
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<Tab>",
+            next = "<M-]>",
+            prev = "<M-[>",
+          },
+        },
+        panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          help = true,
+        },
+      })
+    end,
   },
 }
