cf51a863d2d5b15caab133d30fda96e0912305a5
Colin Graydon <colingraydon@gmail.com>
2025-04-27 15:25:47 -0400

    feat: fix indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,6 +15,11 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+    vim.opt_local.autoindent = true
+    vim.opt_local.smartindent = true
   end,
   desc = "Disable automatic comment continuation",
 })
