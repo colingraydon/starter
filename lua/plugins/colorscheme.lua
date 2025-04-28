23fd9c9294c4befb07134963da06bc3ed8112090
Colin Graydon <colingraydon@gmail.com>
2025-04-27 22:58:48 -0400

    feat: change color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,45 @@
-return {
-  -- Add Dracula colorscheme
-  {
-    "Mofiqul/dracula.nvim",
-    lazy = false,
-    priority = 1000,
-  },
+-- return {
+--   -- Add Dracula colorscheme
+--   {
+--     "Mofiqul/dracula.nvim",
+--     lazy = false,
+--     priority = 1000,
+--   },
+--
+--   -- Configure LazyVim to use Dracula
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "dracula",
+--     },
+--   },
+-- }
 
-  -- Configure LazyVim to use Dracula
+return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dracula",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- Make sure it loads before other plugins
+    config = function()
+      require("catppuccin").setup({
+        -- Customize the theme as needed
+        flavour = "mocha", -- Available options: latte, frappe, macchiato, mocha
+        background = { light = "latte", dark = "mocha" },
+        transparent_background = false,
+        term_colors = true,
+        -- You can customize specific aspects of the theme here
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          treesitter = true,
+          -- For more integrations: https://github.com/catppuccin/nvim#integrations
+        },
+      })
+
+      -- Set colorscheme
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
